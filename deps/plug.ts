0b0fb49eb9b2a775ff45f3c4bab57a030f673e4d
Yohe-Am <56622350+Yohe-Am@users.noreply.github.com>
2023-11-30 17:53:04 +0300

    fix: better `unarchive`

--- deps/plug.ts
+++ deps/plug.ts
@@ -1,5 +1,9 @@
 //! This contains dependencies used by plugins
 
 export * from "./common.ts";
-export * as compress from "https://deno.land/x/compress@v0.4.5/mod.ts";
 export * as zipjs from "https://deno.land/x/zipjs@v2.7.31/index.js";
+export * as compress from "https://deno.land/x/compress@v0.4.5/mod.ts";
+export { Foras } from "https://deno.land/x/foras@v2.1.4/src/deno/mod.ts";
+export * as std_tar from "https://deno.land/std@0.129.0/archive/tar.ts";
+export * as std_streams from "https://deno.land/std@0.129.0/streams/mod.ts";
+export * as std_io from "https://deno.land/std@0.129.0/io/mod.ts";
