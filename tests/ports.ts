acefbc7e1e0a9b7e726e3f72df0da22d93b6f2d0
Yohe-Am <56622350+Yohe-Am@users.noreply.github.com>
2024-02-09 03:11:46 +0000

    fix: bad conditional in meta-cli test

--- tests/ports.ts
+++ tests/ports.ts
@@ -119,7 +119,7 @@ const cases: CustomE2eTestCase[] = [
       ports.meta_cli_ghrel({ full: true }),
       ports.wasmedge(),
     ],
-    ePoint: Deno.env.get("GHJK_TEST_E2E_TYPE") == "docker"
+    ePoint: Deno.env.get("GHJK_TEST_E2E_TYPE") != "local"
       // meta cli runs into segmentation error in the alpine
       // image
       // https://github.com/metatypedev/metatype/issues/584
